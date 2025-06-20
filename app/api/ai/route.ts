807b7dd72fc1606087941da7ec0500ec84bdcd5b
Bhavesh Busa <bhaveshbusa@gmail.com>
2025-06-21 00:02:34 +0100

    Hooking OPEN API

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -1,13 +1,15 @@
 import { OpenAI } from 'openai'
 import { NextResponse } from 'next/server'
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+const openai = new OpenAI({ apiKey: process.env.NEW_OPENAI_API_KEY })
 
 export async function POST(req: Request) {
   const { prompt } = await req.json()
 
+  // return Response.json({ message: `Got prompt: ${prompt}` })
+
   const completion = await openai.chat.completions.create({
-    model: 'gpt-4',
+    model: 'gpt-3.5-turbo',
     messages: [{ role: 'user', content: prompt }],
     functions: [
       {
